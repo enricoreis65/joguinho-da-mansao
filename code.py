ffb80c5f811cc2bd91403b77fb833d040cdb239c
Enrico Borges <enricolbc@al.insper.edu.br>
2020-05-11 18:58:04 -0300

    pulo inicial

--- code.py
+++ code.py
@@ -4,7 +4,12 @@ img_dir = path.join(path.dirname(__file__), 'img')
 pygame.init()
 altura = 480
 largura = 600
-
+STILL = 0
+JUMPING = 1
+FALLING = 2
+GRAVITY=2
+GROUND = altura * 5 // 6
+JUMP_SIZE=20
 # ----- Gera tela principal
 
 window = pygame.display.set_mode((largura, altura))
@@ -27,18 +32,32 @@ class heroi(pygame.sprite.Sprite):
     def update(self):
         # Atualização da posição da nave
         self.rect.x += self.speedx
+
+        self.speedy += GRAVITY
+        # Atualiza o estado para caindo
+        if self.speedy > 0:
+            self.state = FALLING
         self.rect.y += self.speedy
+        # Se bater no chão, para de cair
+        if self.rect.bottom > GROUND:
+            # Reposiciona para a posição do chão
+            self.rect.bottom = GROUND
+            # Para de cair
+            self.speedy = 0
+            # Atualiza o estado para parado
+            self.state = STILL
 
         # Mantem dentro da tela
         if self.rect.right > largura:
             self.rect.right = largura
         if self.rect.left < 0:
             self.rect.left = 0
+    def pulo(self):
+        if self.state == STILL:
+            self.speedy -= JUMP_SIZE
+            self.state = JUMPING
 
-        if self.rect.top< 0:
-            self.rect.top = 0
-        if self.rect.bottom >altura:
-            self.rect.bottom = altura
+        
     
         
     
@@ -69,10 +88,8 @@ while game:
                 player.speedx -= 8
             if event.key == pygame.K_RIGHT:
                 player.speedx += 8
-            if event.key == pygame.K_UP:
-                player.speedy -= 5
-            if event.key == pygame.K_DOWN:
-                player.speedy += 5
+            if event.key == pygame.K_SPACE:
+                    player.pulo()
         # Verifica se soltou alguma tecla.
         if event.type == pygame.KEYUP:
             if event.key in keys_down and keys_down[event.key]:
@@ -81,10 +98,8 @@ while game:
                     player.speedx += 8
                 if event.key == pygame.K_RIGHT:
                     player.speedx -= 8
-                if event.key == pygame.K_UP:
-                    player.speedy += 5
-                if event.key == pygame.K_DOWN:
-                    player.speedy -= 5
+                if event.key == pygame.K_SPACE:
+                    player.pulo()
     all_sprites.update()
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
