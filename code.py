491513401866324954163a6be2baea54ffe0ddff
Enrico Borges <enricolbc@al.insper.edu.br>
2020-05-12 11:01:23 -0300

    menu inicial

--- code.py
+++ code.py
@@ -2,21 +2,28 @@ import pygame
 from os import path
 img_dir = path.join(path.dirname(__file__), 'img')
 pygame.init()
+#-----------------dados iniciais
 altura = 480
 largura = 600
-STILL = 0
-JUMPING = 1
-FALLING = 2
+#----dados movimento
+espera = 0
+pulando = 1
+caindo = 2
 gravidade=2
 chao = altura * 5 // 6
-tamanho_do_pulo=20
+tamanho_do_pulo=18
+
+
+
 # ----- Gera tela principal
 
 window = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('mansao')
 
+#definindo o player
+
 heroi_largura=30
-heroi_altura=50
+heroi_altura=40
 player_img = pygame.image.load(path.join(img_dir, 'hero-single.png')).convert_alpha()
 player_img = pygame.transform.scale(player_img, (heroi_largura, heroi_altura))
 
@@ -29,6 +36,8 @@ class heroi(pygame.sprite.Sprite):
         self.rect.bottom = altura -1
         self.speedx = 0
         self.speedy=0
+
+    #update    
     def update(self):
         # Atualização da posição do heroi
         self.rect.x += self.speedx
@@ -36,28 +45,86 @@ class heroi(pygame.sprite.Sprite):
         self.speedy += gravidade
         # Atualiza o estado para caindo
         if self.speedy > 0:
-            self.state = FALLING
+            self.state = caindo
         self.rect.y += self.speedy
         # Se bater no chão, para de cair
         if self.rect.bottom > chao:
             # Reposiciona para a posição do chão
             self.rect.bottom = chao
+
             # Para de cair
             self.speedy = 0
             # Atualiza o estado para parado
-            self.state = STILL
+            self.state = espera
 
         # Mantem dentro da tela
         if self.rect.right > largura:
             self.rect.right = largura
         if self.rect.left < 0:
             self.rect.left = 0
-    def pulo(self):
-        if self.state == STILL:
-            self.speedy -= tamanho_do_pulo
-            self.state = JUMPING
 
-        
+    def pulo(self):
+        if self.state == espera:
+            self.speedy -= tamanho_do_pulo
+            self.state = pulando
+
+class modo_de_jogo():
+    def __init__(self):
+        self.aba="menu"
+    def jogando(self):
+        for event in pygame.event.get():
+        # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                game = False
+                pygame.quit() 
+        # Verifica se apertou alguma tecla.
+            if event.type == pygame.KEYDOWN:
+                keys_down[event.key] = True
+            # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    player.speedx -= 4
+                if event.key == pygame.K_RIGHT:
+                    player.speedx += 4
+                if event.key == pygame.K_SPACE:
+                    player.pulo()
+        # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                if event.key in keys_down and keys_down[event.key]:
+            # Dependendo da tecla, altera a velocidade.
+                    if event.key == pygame.K_LEFT:
+                        player.speedx += 4
+                    if event.key == pygame.K_RIGHT:
+                        player.speedx -= 4
+                
+        all_sprites.update()
+    # ----- Gera saídas
+        window.fill((0, 0, 0))  # Preenche com a cor branca
+        all_sprites.draw(window)
+    # ----- Atualiza estado do jogo
+        pygame.display.update()   
+    def menu(self):
+        for event in pygame.event.get():
+        # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                game = False
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                keys_down[event.key] = True
+                if event.key==pygame.K_p:
+                    self.aba="jogando"
+        window.fill((255, 255, 255))
+        pygame.display.update() 
+
+    def controlador_menu(self):
+        if self.aba=="menu":
+            self.menu()
+        if self.aba=="jogando":
+            self.jogando()
+
+
+
+            
+
     
         
     
@@ -65,9 +132,9 @@ class heroi(pygame.sprite.Sprite):
 # ----- Inicia estruturas de dados
 
 clock = pygame.time.Clock()
-FPS = 30
+FPS = 60
 all_sprites = pygame.sprite.Group()
-
+estado_do_jogo= modo_de_jogo()
 player= heroi(player_img)
 all_sprites.add(player)
 keys_down = {}
@@ -75,41 +142,8 @@ game=True
 # ===== Loop principal =====
 while game:
     clock.tick(FPS)
-    # ----- Trata eventos
-    for event in pygame.event.get():
-        # ----- Verifica consequências
-        if event.type == pygame.QUIT:
-            game = False
-        # Verifica se apertou alguma tecla.
-        if event.type == pygame.KEYDOWN:
-            keys_down[event.key] = True
-            # Dependendo da tecla, altera a velocidade.
-            if event.key == pygame.K_LEFT:
-                player.speedx -= 8
-            if event.key == pygame.K_RIGHT:
-                player.speedx += 8
-            if event.key == pygame.K_SPACE:
-                    player.pulo()
-        # Verifica se soltou alguma tecla.
-        if event.type == pygame.KEYUP:
-            if event.key in keys_down and keys_down[event.key]:
-            # Dependendo da tecla, altera a velocidade.
-                if event.key == pygame.K_LEFT:
-                    player.speedx += 8
-                if event.key == pygame.K_RIGHT:
-                    player.speedx -= 8
-                if event.key == pygame.K_SPACE:
-                    player.pulo()
-    all_sprites.update()
-    # ----- Gera saídas
-    window.fill((0, 0, 0))  # Preenche com a cor branca
+    estado_do_jogo.controlador_menu()
     
-    all_sprites.draw(window)
-
-    
-    # ----- Atualiza estado do jogo
-    pygame.display.update()  # Mostra o novo frame para o jogador
-
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
 
