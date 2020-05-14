89553e36829a167f86f5408325033036608da770
Enrico Borges <enricolbc@al.insper.edu.br>
2020-05-14 16:08:01 -0300

    carra vida inimigo e vida inimigo

--- code.py
+++ code.py
@@ -111,6 +111,7 @@ class heroi(pygame.sprite.Sprite):
         if estado_do_jogo.aba=="jogando":
             if player.vida<=0:
                 player.kill()
+                barra.kill()
                 
             else:
                 if player.estado==indefeso:
@@ -119,22 +120,25 @@ class heroi(pygame.sprite.Sprite):
                     if len(colisao)>0:
                         player.hora_do_dano=pygame.time.get_ticks()
                         player.estado=tomando_dano
-                        player.vida-=10
-                        colisao.clear()
+                        player.vida-=10                       
                         player.rect.x-=60
                         player.rect.y-=tamanho_do_pulo
                     
             
                 if player.estado==ataque:
-                    colisao=pygame.sprite.spritecollide(player,all_enemis,True)
+                    colisao=pygame.sprite.spritecollide(player,all_enemis,False)
                     
                     if len(colisao)>0:
-                        player.vida+=1
-
+                        colisao.clear()
+                        inimigos(inimigos_img,player,vida_inimigo).dano()
+                        player.rect.x-=8
+                        player.rect.y-=3
+                        
+                        vida_verm(barra_vermelha_img,inimigo,barra_largura).diminuir()
         
 
 class inimigos(pygame.sprite.Sprite):
-    def __init__(self,img,player):
+    def __init__(self,img,player,vida_inimigo):
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.rect = self.image.get_rect()
@@ -142,8 +146,9 @@ class inimigos(pygame.sprite.Sprite):
         self.rect.bottom = chao
         self.speedx_inimigo = 0
         self.speedy_inimigo= 0
-        
+        self.vida=vida_inimigo
     def update(self):
+        
         self.rect.x += self.speedx_inimigo
         self.rect.y += self.speedy_inimigo
         
@@ -163,6 +168,16 @@ class inimigos(pygame.sprite.Sprite):
 
         if player.rect.y-self.rect.y<0 :
             self.speedy_inimigo = -1
+    def dano(self):
+        inimigo.vida-=10
+        inimigo.rect.x+=30
+        inimigo.rect.y-=15
+        if inimigo.vida==0:
+            inimigo.kill()
+            barra_vermelha.kill()
+        
+        
+
 
 
 class modo_de_jogo():
@@ -290,9 +305,30 @@ class stamina(pygame.sprite.Sprite):
                 self.largura=32
                 self.image=pygame.transform.scale(self.image2, (self.largura2, barra_altura))
 
+class vida_verm(pygame.sprite.Sprite):
+    def __init__(self,img,inimigo,largura):
+        pygame.sprite.Sprite.__init__(self)
+        vida_verm.image = img
+        
+
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 0
+        self.rect.bottom = 0
+        self.largura=largura
+        
+
+    def update(self):
+        
+        self.rect.centerx = inimigo.rect.centerx
+        self.rect.bottom = inimigo.rect.bottom-heroi_altura-2
+    def diminuir(self):
+            self.largura-=10
+            vida_verm.image=pygame.transform.scale(self.image, (self.largura, barra_altura))
+        
 
 
-
+        
+        
            
 
 
@@ -302,16 +338,19 @@ class stamina(pygame.sprite.Sprite):
 
 clock = pygame.time.Clock()
 vida=100
+vida_inimigo=50
 FPS = 60
 all_sprites = pygame.sprite.Group()
 all_enemis = pygame.sprite.Group()
 
 player= heroi(player_img,vida,teste_img)
 estado_do_jogo= modo_de_jogo(player)
-inimigo= inimigos(inimigos_img,player)
+inimigo= inimigos(inimigos_img,player,vida_inimigo)
 barra= stamina(barra_img,player,barra_largura)
+barra_vermelha=vida_verm(barra_vermelha_img,inimigo,barra_largura)
 all_sprites.add(player)
 all_sprites.add(barra)
+all_sprites.add(barra_vermelha)
 all_sprites.add(inimigo)
 all_enemis.add(inimigo)
 keys_down = {}
@@ -323,4 +362,4 @@ while game:
     clock.tick(FPS)
     estado_do_jogo.controlador_menu()
     agora=pygame.time.get_ticks() 
-      
+    
